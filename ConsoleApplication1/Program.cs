cdb921bd1a59722957a0a1c6156bf4e54382a88e
MaximusB93 <maksim.budnikov.github@gmail.com>
2023-03-24 15:50:46 +0300

    Первый комит Branch2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApplication1
         public static void Main(string[] args)
         {
             Console.WriteLine("Привет мир");
+            Console.WriteLine("Ветка 2");
         }
     }
 }
